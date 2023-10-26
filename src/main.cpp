897f1d0ed9bb4296964712132714300262265064
Jacky Sun <jackysunhz@126.com>
2023-10-26 16:22:30 -0400

    Initial CMake Set Up(Requires Vulkan SDK, glm and glfw)

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
 ﻿// RealTimeRenderer.cpp : Defines the entry point for the application.
 //
 
+#include <cstdlib>
 #include <iostream>
+#include <stdexcept>
+
+#include "HelloTriangleProgram.hpp"
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	HelloTriangleApplication app;
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
